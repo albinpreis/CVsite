e598e3a4627643d55ca051055d83f88bcaa8e0da
Albin Preis <albin.preis@gmail.com>
2021-12-17 14:39:23 +0100

    Add project files.

---
+++ CVsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CVsite.Startup))]
+namespace CVsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
