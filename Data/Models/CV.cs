ba8e383cb6aa1826894e8251fb4e9d914296e394
amandamathisen <amandamathisen@hotmail.com>
2021-12-19 17:40:28 +0100

    public

--- Data/Models/CV.cs
+++ Data/Models/CV.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Data.Models
 {
-    internal class CV
+    public class CV
     {
         public int ID { get; set; }
         public virtual User user { get; set; }
