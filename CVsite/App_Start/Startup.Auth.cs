d82e8294f7bda4decac858f14a267829827f0554
Albin Preis <albin.preis@gmail.com>
2021-12-19 17:15:13 +0100

    fix

--- CVsite/App_Start/Startup.Auth.cs
+++ CVsite/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using CVsite.Models;
+using Data;
 
 namespace CVsite
 {
--- CVsite/Controllers/AccountController.cs
+++ CVsite/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using CVsite.Models;
+using Data;
 
 namespace CVsite.Controllers
 {
