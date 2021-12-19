910cf9ec9286147e79e80f40904a294b08f6e883
amandamathisen <amandamathisen@hotmail.com>
2021-12-19 17:39:36 +0100

    cv klass

---
+++ Data/Models/CV.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    internal class CV
+    {
+        public int ID { get; set; }
+        public virtual User user { get; set; }
+        public string kompetens { get; set; }
+    }
+}
