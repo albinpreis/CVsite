2a21c72ccbbea9bed9b885ba42eb1c5111df8538
Albin Preis <albin.preis@gmail.com>
2021-12-19 17:40:05 +0100

    user

--- Data/Models/User.cs
+++ Data/Models/User.cs
@@ -11,11 +11,14 @@ namespace Data.Models
     {
         [Key]
         public int ID { get; set; }
-
         public string UserName  { get; set; }
         public string Password { get; set; }
         public string ImagePath { get; set; }
         public string Address { get; set; }
+        public bool PrivateUser { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public virtual ICollection<CV> Cvs { get; set; }
 
     }
 }
